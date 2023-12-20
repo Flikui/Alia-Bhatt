1cf24672f4f07a2e5b1c8239af413a1d9bf230d6
Flikui <154420499+Flikui@users.noreply.github.com>
2023-12-20 18:15:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "True"), True)
 PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
 PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
-LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n👨‍💻Dᴇᴠ : {}\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🛠️Bᴜɪʟᴅ Sᴛᴀᴛᴜꜱ V4.5.0 [ꜱᴛᴀʙʟᴇ]"
+LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n👨‍💻Dᴇᴠ : {"1861354020"}\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n🛠️Bᴜɪʟᴅ Sᴛᴀᴛᴜꜱ V4.5.0 [ Sᴛᴀʙʟᴇ ]"
 
 
 
